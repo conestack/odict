beb18ff1925b4cc9d3f5c4c5c5d88c284dd348c0
rnix <rnix@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2009-03-17 10:17:22 +0000

    add odict as seperate egg
    git-svn-id: https://svn.plone.org/svn/archetypes/AGX/odict/trunk@10755 12379f12-61e2-0310-a953-c5ab0a7db1c5

---
+++ README.txt
@@ -0,0 +1,11 @@
+
+Yet another odict implementation.
+
+  >>> from odict import odict
+  >>> odict = odict()
+  
+Credits:
+--------
+
+  Written by bearophile (Oct 12 2006)
+  Eggified by rnix (Mar 17 2009)
---
+++ setup.py
@@ -0,0 +1,33 @@
+# Copyright 2003-2009, BlueDynamics Alliance - http://bluedynamics.com
+# GNU General Public License Version 2 or later
+
+from setuptools import setup, find_packages
+import sys, os
+
+shortdesc = 'Ordered dictionary.'
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+
+setup(name='odict',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc + todo + changes,
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: Python Software Foundation License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python', 
+            'Topic :: Software Development',       
+      ],
+      keywords='odict',
+      author='bearophile, rnix',
+      author_email='rnix@squarewave.at',
+      url=u'https://svn.plone.org/svn/archetypes/AGX/odict',
+      license='GNU General Public Licence',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=['setuptools'],
+      extras_require = {},
+      )
