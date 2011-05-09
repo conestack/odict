cae6758e53b071d978baa97a825df2fb0165b965
Robert Niederreiter <rnix@squarewave.at>
2011-05-09 11:24:45 +0200

    no coverage in module __main__

--- src/odict/tests.py
+++ src/odict/tests.py
@@ -24,4 +24,4 @@ def test_suite():
     ])
 
 if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
+    unittest.main(defaultTest='test_suite')                 #pragma NO COVERAGE
