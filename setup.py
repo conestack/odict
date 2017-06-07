6975bf37d08764d74a5eeea017708ffa92c4212e
Robert Niederreiter <office@squarewave.at>
2017-06-07 12:31:18 +0200

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.6.1'
+version = '1.6.2.dev0'
 shortdesc = 'Ordered Dictionary.'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
