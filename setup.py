a6a42969cd8a24670532a2a86d02a6e0f6b6e9b4
Robert Niederreiter <office@squarewave.at>
2021-10-21 17:27:18 +0200

    classifiers

--- setup.py
+++ setup.py
@@ -37,8 +37,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development',
     ],
     keywords='odict dict ordered dictionary mapping collection tree',
