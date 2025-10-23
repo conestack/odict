8f171e8c7141274b132df6c5b16f1f78dac39ba3
Robert Niederreiter <rnix@squarewave.at>
2025-10-23 19:01:58 +0200

    build from pyproject.toml

--- include.mk
+++ include.mk
@@ -2,20 +2,11 @@
 # codict - Cython extension
 ##############################################################################
 
-# Flag whether to build Cython extensions
-# Default: true
-BUILD_CODICT?=true
-
 CODICT_TARGET:=$(SENTINEL_FOLDER)/codict.sentinel
 $(CODICT_TARGET): $(PACKAGES_TARGET)
-ifeq ("$(BUILD_CODICT)", "true")
 	@echo "Building Cython codict extension"
-	@$(MXENV_PYTHON) setup.py build_ext --inplace
+	@hatch build
 	@touch $(CODICT_TARGET)
-else
-	@echo "Skipping Cython codict extension build"
-	@touch $(CODICT_TARGET)
-endif
 
 .PHONY: codict
 codict: $(CODICT_TARGET)
@@ -30,9 +21,6 @@ codict-clean: codict-dirty
 	@rm -f src/odict/codict.c
 	@rm -f src/odict/*.so
 	@rm -f src/odict/*.pyd
-	@rm -rf build/
-	@find . -name '*.pyc' -delete
-	@find . -name '__pycache__' -type d -exec rm -rf {} + 2>/dev/null || true
 
 INSTALL_TARGETS+=$(CODICT_TARGET)
 DIRTY_TARGETS+=codict-dirty
